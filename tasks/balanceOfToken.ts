5c2bb318b9440e94d50b425f4efef7919677a191
blockedby <66838646+blockedby@users.noreply.github.com>
2022-01-27 11:40:08 +0300

    balance works

---
+++ tasks/balanceOfToken.ts
@@ -0,0 +1,13 @@
+import { task } from "hardhat/config";
+
+task("balanceOfToken", "Approves account to transferFrom")
+    .addParam("token", "deployed contract addresss")
+    .addParam("of", "balance of address")
+    .setAction(async (taskArgs, hre) => {
+        const token = await hre.ethers.getContractAt("ZepToken", taskArgs.token);
+        const of = hre.ethers.utils.getAddress(taskArgs.of);
+        // do not work without it
+        const balance = await token.balanceOf(of);
+        console.log(balance.toNumber());
+
+    });
